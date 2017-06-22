3ef7f2f1e2c550a48c293e3388d634ec29a6ef97
Russ Perlow <rep4975@rit.edu>
2017-06-22 15:10:30 -0400

    Initial Commit

---
+++ Delegation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Delegation
+//
+//  Created by Russ Perlow on 6/22/17.
+//  Copyright © 2017 Russ Perlow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
